2ba5474e5d1b2e0e938db7714c39787b19894688
Ninh Thai Lam <thailamstudy@gmail.com>
2024-08-02 17:39:45 +0700

    Read the prop inside the child component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,41 @@
 
-function Avatar () {
+function Avatar ({person, size}: AvatarProp) {
   return (
     <img 
     className="avatar"
-    src="https://i.imgur.com/1bX5QH6.jpg" 
-    alt="Someone that I don't know" 
-    height={100}
-    width={100}
+    src={"https://i.imgur.com/" + person.imageID + ".jpg" }
+    alt={person.name}
+    height={size}
+    width={size}
+    
     />
   )
+}
+type PersonPerop = {
+  name: string,
+  imageID: string
+}
+
+type AvatarProp = {
+  person: PersonPerop
+  size: number
 }
 
 export default function Display() {
   return (
     <>
-      <Avatar />
+      <Avatar 
+        person={{name: 'Person1', imageID: 'OKS67lh'} }
+        size={100}
+      />
+      <Avatar 
+        person={{name: 'Person2', imageID: 'YfeOqp2'} }
+        size={80}
+      />
+      <Avatar 
+        person={{name: 'Person3', imageID: '1bX5QH6'} }
+        size={50}
+      />
     </>
   )
 }
